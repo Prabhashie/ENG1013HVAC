1112839de276f0aa6a9eeff9639c120fd6fe0fc8
Erim Can <ecan0004@student.monash.edu>
2023-07-04 12:35:44 +1000

    Option Selection

--- MVP/main.py
+++ MVP/main.py
@@ -1,2 +1,3 @@
 print("Welcome to ENG1013 Smart Fan System")
-print("Please select from the below options:")
+print("Please select from the below options:\nControl System: 1\nSystem Settings: 2\nGraphing: 3 \n")
+user_Input = input("Input:")
