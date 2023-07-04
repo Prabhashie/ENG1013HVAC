1feed128ac121f2211ab951b907c48d178649976
Erim Can <ecan0004@student.monash.edu>
2023-07-04 12:47:47 +1000

    Code for menu selection

--- MVP/main.py
+++ MVP/main.py
@@ -1,3 +1,17 @@
 print("Welcome to ENG1013 Smart Fan System")
 print("Please select from the below options:\nControl System: 1\nSystem Settings: 2\nGraphing: 3 \n")
-user_Input = input("Input:")
+while True:
+    user_Input = input("Input:")
+    if user_Input != 1 or 2 or 3:
+        print("Invalid selection! Please choose again.")
+        continue
+    else:
+        break
+
+def main():
+    if user_Input == 1:
+        #call the relevant function
+    elif user_Input == 2:
+        #call the relevant function
+    elif user_Input == 3:
+        #call the relevant function
